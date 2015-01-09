842d7e56edb0b579ff8d33c3f5fd3a12afd8acfd
foulbricks <foulbricks@gmail.com>
2015-01-09 01:44:46 -0500

    Allow nil numbers on db validation

--- app/models/setup.rb
+++ app/models/setup.rb
@@ -6,7 +6,7 @@ class Setup < ActiveRecord::Base
             :building_id, presence: true, on: :update
             
   validates :mav_expiration, :invoice_generation, :invoice_delivery, 
-            :numericality => {:only_integer => true, :minimum => 1, :maximum => 28}
+            :numericality => {:only_integer => true, :minimum => 1, :maximum => 28}, :allow_nil => true
             
   validates :building_id, presence: true, on: :create
 
