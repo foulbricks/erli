259f5ff8ea7df6c20dceae093d07d941facc37dd
foulbricks <foulbricks@gmail.com>
2015-03-11 08:52:50 -0400

    Set up smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'email-smtp.us-east-1.amazonaws.com',
+    :authentication => :login,
+    :user_name => ENV["SMTP_USERNAME"],
+    :password => ENV["SMTP_PASSWORD"],
+    :enable_starttls_auto => true,
+    :port => 465
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
